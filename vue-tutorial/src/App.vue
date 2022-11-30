63bdcbffb1741ab9be6271e33ade2582c0b8f978
Nidhi Desu <nd2290@nyu.edu>
2022-11-30 18:36:15 -0500

    initial commit

---
+++ vue-tutorial/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+   <UserProfile/>
+  </div>
+</template>
+
+<script>
+import UserProfile from './components/UserProfile.vue';
+
+
+export default {
+    name: "App",
+    
+    components: { UserProfile }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+  min-height: 100vh;
+  background-color: #F3F5FA;
+}
+</style>
